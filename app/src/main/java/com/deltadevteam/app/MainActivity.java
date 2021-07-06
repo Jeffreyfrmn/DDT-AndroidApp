7e01bbd8692e52b80995cfe2777de83fec1549b6
jeffreyfrmn <jeffreywaynefreemanjr@gmail.com>
2021-07-06 16:12:15 -0700

    "Scan for Devices" button for activity_main.xml + MainActivity.java implemented.

--- app/src/main/java/com/deltadevteam/app/MainActivity.java
+++ app/src/main/java/com/deltadevteam/app/MainActivity.java
@@ -3,6 +3,8 @@ package com.deltadevteam.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void scanButton(View view) {
+        TextView scanResult = findViewById(R.id.scanResult);
+
+        if (scanResult.getText().toString().equals("No Devices Found.")) {
+
+        } else {
+
+        }
     }
 
 }
