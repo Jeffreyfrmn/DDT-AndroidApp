839705794920ec37c1ae12268d1fd91c820178ff
jeffreyfrmn <jeffreywaynefreemanjr@gmail.com>
2021-07-07 16:04:24 -0700

    BlueSTSDK.Manager mngr defined and TODOs outlined.

--- app/src/main/java/com/deltadevteam/app/MainActivity.java
+++ app/src/main/java/com/deltadevteam/app/MainActivity.java
@@ -1,11 +1,20 @@
 package com.deltadevteam.app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
 import com.st.BlueSTSDK.*;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +25,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void scanButton(View view) {
+        final int SCANNING_TIME = 1500;
+
         TextView scanResult = findViewById(R.id.scanResult);
+        TextView nodeResult = findViewById(R.id.nodeResult);
+        Manager mngr = Manager.getSharedInstance();
+        Manager.ManagerListener listener = new Manager.ManagerListener() {
+            @Override
+            public void onDiscoveryChange(
+                    @NonNull @NotNull Manager mngr,
+                    @NonNull @NotNull boolean enabled ) {
+                String res = "Discovery " + (enabled ? "started" : "stopped") + ".\n";
+                scanResult.setText(res);
+            }
 
-        if (scanResult.getText().toString().equals("No Devices Found.")) {
+            @Override
+            public void onNodeDiscovered(
+                    @NonNull @NotNull Manager mngr,
+                    @NonNull @NotNull Node node ) {
+                String res = "Device Discovered: " + node.getName() + '\n';
+                res = nodeResult.getText().toString() + res;
+                nodeResult.setText(res);
+            }
+        };
+        mngr.addListener(listener);
 
+        //TODO: handle mngr.startDiscovery w/ Permissions
+        //mngr.startDiscovery(SCANNING_TIME);
+
+        List<Node> nodes = mngr.getNodes();
+
+        if (nodes.isEmpty()) {
+            scanResult.setText("No devices found.");
         } else {
-
+            //TODO: Launch new Activity (ConnectActivity) which handels device listings.
         }
     }
 
