cd5c8e359547a5d44f5f101aabaca235b148b31b
jeffreyfrmn <jeffreywaynefreemanjr@gmail.com>
2021-07-09 17:25:58 -0700

    v0.01 Resolved Issues:  - #1 "Build Process to Debug using Android Hardware"  - #2 "v0.01 DDT-AndroidApp Running on Tablet"  - #3 "ReadMe of Development Environment Setup"
     This is software release: DDT-AndroidApp_v0.01

---
+++ README.md
@@ -0,0 +1,52 @@
+# README
+
+This README documents the steps that are necessary to get the application up and running for software development.
+
+Package: ***com.deltadevteam.app***
+
+### What is this repository for?
+> This repository is for development, debugging, and validation of Delta Development Team's INC. general purpose android application.  This software is used to view data from Delta Development Team, INC. Bluetooth enabled devices, running the 'BlueST Protocol'.
+
+## Requirements Before Development
+- A BlueST Protocol Enabled *sending* Device (*ex. APRU, TBS*)
+- An Android *receiving* Device (*ex. Phone or Tablet*) configured for development.
+- Permissions to clone this repository.
+
+## Developer Installation
+1. [Download and Install **Git** Version Controll.](https://git-scm.com/downloads)
+2. [Download and Install the **Android Studio IDE**.](https://developer.android.com/studio)
+3. **Launch the Android Studio IDE** and navigate to:
+```File -> New --> Project from Version Control...```
+4. When the *Get from Version Control* window appears, **paste the following address into the URL field**: ```https://github.com/Jeffreyfrmn/DDT-AndroidApp.git``` and (*if applicable*) set the desired directory the repository will be cloned to.
+> Note: Ensure you have the available permissions to clone the above repository.
+
+### Configuring Android Devices for App Development
+> Android Studio IDE offers use of Android Virtual Machines, however since Bluetooth is a hardware feature, requiring permissions and complexities that may create issues within the development cycle, we have opted to debug this application with hardware connected directly to the IDE.
+
+#### Using Lenovo M8 Tablet for Development
+Currently we use the [Lenovo M8 Tablet](https://www.lenovo.com/us/en/tablets/android-tablets/lenovo-tab-series/Lenovo-TB-8505/p/ZZITZTATB58) for production and development purposes at Delta Development Team. Below outlines the process to configure the tablet for Development Mode and USB Debugging permissions.
+1. Touch **Advanced Menu on Home screen**.
+2. Tap **Settings**.
+3. Tap **About tablet**.
+4. Seven taps on **Build number** until “You are now a developer” or “No need, you are already a developer” pops out.
+5. Tap **Developer options** on the Settings menu.
+6. Turn on **USB debugging**.
+7. Tap **OK** to allow USB debugging.
+
+#### Purchasing Lenovo M8 Tablet for Development
+- [Lenovo M8 Tablet](https://www.lenovo.com/us/en/tablets/android-tablets/lenovo-tab-series/Lenovo-TB-8505/p/ZZITZTATB58)
+- [ProCase for M8 Tablet](https://www.amazon.com/ProCase-Shockproof-Kickstand-Protective-TB-8505FS/dp/B08LMCW7KJ/ref=sr_1_6?crid=1SYAN99MEJA7L&dchild=1&keywords=lenovo+m8+tablet+case&qid=1625875922&sprefix=lenovo+m8+t%2Caps%2C207&sr=8-6)
+- Micro USB Cable (*used for debugging*)
+
+### Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+### Contribution Guidelines
+> - Do not change repository gradle files.
+> - All directly editable backend (java/kotlin) files are contained in **\src**
+> - Repo Contribution Procedures: [See SOP-17 Software Validation](https://drive.google.com/file/d/1miasyqhBxNCYkjjHyrdOgMqzPbwDj2aI/view?usp=sharing)
+
+### Who do I talk to?
+> Repo Owner: [Jeffrey Freeman](montio:jfreeman@deltadevteam.com) (Software Lead)
+
+## License
--- app/src/main/java/com/deltadevteam/app/MainActivity.java
+++ app/src/main/java/com/deltadevteam/app/MainActivity.java
@@ -1,14 +1,20 @@
 package com.deltadevteam.app;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 //import com.st.BlueSTSDK.*;
 import com.st.BlueSTSDK.Manager;
@@ -19,86 +25,110 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private int ACCESS_FINE_LOCATION_PERMISSION_CODE = 1;
+
+    final int SCANNING_TIME = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void scanButton(View view) {
-        final int SCANNING_TIME = 1500;
 
         TextView scanResult = findViewById(R.id.scanResult);
-        TextView nodeResult = findViewById(R.id.nodeResult);
 
-        Manager mngr = Manager.getSharedInstance();
-        //mngr.addListener(getListener(scanResult, nodeResult));
-
-        Manager.ManagerListener listener = new Manager.ManagerListener() {
+        Button scanButton = findViewById(R.id.scanButton);
+        // TODO: If clicked again, mngr.resetDiscovery().
+        scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDiscoveryChange(
-                    @NonNull @NotNull Manager mngr,
-                    @NonNull @NotNull boolean enabled) {
-                String res = "Discovery " + (enabled ? "started" : "stopped") + ".\n";
-                scanResult.setText(res);
+            public void onClick(View view) {
+
+                Manager mngr = Manager.getSharedInstance();
+
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    //Toast.makeText(MainActivity.this, "Permission has already been granted.", Toast.LENGTH_SHORT).show();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Manager.ManagerListener listener = new Manager.ManagerListener() {
+                                @Override
+                                public void onDiscoveryChange(
+                                        @NonNull @NotNull Manager mngr,
+                                        @NonNull @NotNull boolean enabled) {
+                                    String res = "Discovery " + (enabled ? "started" : "stopped") + ".\n";
+                                    System.out.println(res);
+                                }
+
+                                @Override
+                                public void onNodeDiscovered(
+                                        @NonNull @NotNull Manager mngr,
+                                        @NonNull @NotNull Node node) {
+                                    String res = "Device Discovered: " + node.getName() + '\n';
+                                    System.out.println(res);
+                                }
+                            };
+                            mngr.addListener(listener);
+                        }
+                    });
+
+                    System.out.println("Started mngr.startDiscovery()");
+                    scanResult.setText("Discovery started:\n");
+
+                    mngr.startDiscovery(5000);
+
+                    if (mngr.getNodes().isEmpty()) {
+                        scanResult.setText("No Devices Found.");
+                    } else {
+                        for (Node node : mngr.getNodes()) {
+                            String msg = scanResult.getText().toString() + "Found Node: " + node.getName() + "\n";
+                            System.out.println(msg);
+                            scanResult.setText(msg);
+
+                            // TODO: Connect to Node...
+                        }
+                    }
+                } else {
+                    requestBltPermission();
+                }
             }
+        });
+    }
 
-            @Override
-            public void onNodeDiscovered(
-                    @NonNull @NotNull Manager mngr,
-                    @NonNull @NotNull Node node) {
-                String res = "Device Discovered: " + node.getName() + '\n';
-                res = nodeResult.getText().toString() + res;
-                nodeResult.setText(res);
-            }
-        };
+    private void requestBltPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
 
-        //TODO: handle mngr.startDiscovery w/ Permissions
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission Needed")
+                    .setMessage("In order to connect to Delta Development Team Devices, bluetooth permissions are needed.")
+                    .setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_FINE_LOCATION_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
         } else {
-            mngr.startDiscovery(SCANNING_TIME);
-        }
-
-        List<Node> nodes = mngr.getNodes();
-
-        if (nodes.isEmpty()) {
-            scanResult.setText("No devices found.");
-        } else {
-            //TODO: Launch new Activity (ConnectActivity) which handels device listings.
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_FINE_LOCATION_PERMISSION_CODE);
         }
     }
 
-    /*
-    private Manager.ManagerListener getListener(TextView scanResult, TextView nodeResult) {
-        Manager.ManagerListener listener = new Manager.ManagerListener() {
-            @Override
-            public void onDiscoveryChange(
-                    @NonNull @NotNull Manager mngr,
-                    @NonNull @NotNull boolean enabled ) {
-                String res = "Discovery " + (enabled ? "started" : "stopped") + ".\n";
-                scanResult.setText(res);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == ACCESS_FINE_LOCATION_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
             }
-
-            @Override
-            public void onNodeDiscovered(
-                    @NonNull @NotNull Manager mngr,
-                    @NonNull @NotNull Node node ) {
-                String res = "Device Discovered: " + node.getName() + '\n';
-                res = nodeResult.getText().toString() + res;
-                nodeResult.setText(res);
-            }
-        };
-        return listener;
+        }
     }
-    */
 
 }
